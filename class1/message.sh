d8cf59f34d772676444d841fc3bea052600d51ea
EC2 Default User <ec2-user@ip-172-31-81-124.ec2.internal>
2024-12-05 14:40:33 +0000

    This is Bash classes

---
+++ class1/message.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+name=Kairat
+echo "Hello my name is $name"
+echo "$name is currently studying Linux"
+echo "Kairat loves DevOps"
+echo "26*30*12"
+
---
+++ class2/weather.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Enter temperature: " temp
+
+if [ $temp -le 40 ]
+then
+	echo "Cold"
+
+elif [ $temp -gt 40 ] && [ $temp -le 60 ]
+then
+	echo "Cool"
+
+elif [[ $temp -gt 60 && $temp -le 80 ]]
+then
+	echo "Warm"
+
+elif [ $temp -le 30 ]
+then
+	echo "Freezing"
+
+else [ $temp -gt 80 ]
+	echo "Blasting"
+fi
